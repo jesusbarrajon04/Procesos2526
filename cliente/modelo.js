d558d4bc847a42e6bdd5687ed03eb0d90cca0fc9
Jesus Fernando Barrajon Lopez <jesusfernando.barrajon@alu.uclm.es>
2025-10-07 09:01:40 +0200

    Bloque 2. Implementación del modelo y proyecto de pruebas

---
+++ cliente/modelo.js
@@ -0,0 +1,28 @@
+function Sistema() {
+  this.usuarios = {};
+
+  this.agregarUsuario = function(nick) {
+    this.usuarios[nick] = new Usuario(nick);
+  };
+
+  this.obtenerUsuarios=function(){ 
+    return this.usuarios;
+  }
+
+  this.usuarioActivo = function(nick) {
+    return nick in this.usuarios;
+  };
+
+  this.eliminarUsuario = function(nick) {
+    delete this.usuarios[nick];
+  };
+
+  this.numeroUsuarios = function() {
+    return Object.keys(this.usuarios).length;
+  };
+
+}
+
+function Usuario(nick) {
+  this.nick = nick;
+}
